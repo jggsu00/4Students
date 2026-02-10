1ca17e6584405ae166ecfdee6ac07c237f73a9f7
John Gomez <johngomez45@Johns-MacBook-Pro.local>
2026-02-10 15:09:51 -0500

    added sign out button, base is ready

--- lib/qr_test_screen.dart
+++ lib/qr_test_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'login_screen.dart';
 
 class QrTestScreen extends StatelessWidget {
   QrTestScreen({super.key});
@@ -8,11 +10,26 @@ class QrTestScreen extends StatelessWidget {
   final String randomData =
       'TEST_CHECK_IN_${Random().nextInt(999999)}';
 
+  Future<void> _signOut(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (_) => const LoginScreen()),
+          (route) => false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Test QR Generator'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => _signOut(context),
+          ),
+        ],
       ),
       body: Center(
         child: Column(
